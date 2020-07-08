e98d5bf0294c48ea3d9bbc902ac45721489e87f3
Aleksandar Lakatoš <lakatos.aleksandar79@gmail.com>
2020-07-08 23:57:34 +0200

    Navigation

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,14 +1,11 @@
 <div id="navigation">
     <h2>Saša Stojanović</h2>
     <ul id="list" class="list-group">
-        <li class="list-group-item">
-            <a href="" class="btn">Lista klijenata</a>
+        <li class="list-group-item active">
+            <a href="/" class="button"><i class="fas fa-users"></i>Lista klijenata</a>
         </li>
         <li class="list-group-item">
-            <a href="" class="btn"></a>
-        </li>
-        <li class="list-group-item">
-            <a href="" class="btn"></a>
+            <a href="/user/create" class="button"><i class="fas fa-user-plus"></i>Dodaj klijenta</a>
         </li>
     </ul>
 </div>
